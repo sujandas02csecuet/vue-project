0bc05fa4c01b0653a5ce532606b369ed28e3f1a4
Administrator <Administrator@SUJANHOME>
2024-02-25 11:38:00 +0600

    xf

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,3 +1,68 @@
-<template></template>
-<script></script>
+<template>
+
+<div class="container">
+    <h3>All Students</h3>
+    <div v-if="message" class="alert alert-success">{{this.message}}</div>
+
+<div>
+    <button class="btn btn-success" v-on:click="addStudent()">Add Student</button>
+
+<template>
+    <router-view></router-view>
+</template>
+</div>
+
+<br/>
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Present Address</th>
+            <th>Roll Number</th>
+            <th>Contact No</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="student in students" v-bind:key="student.id">
+        <td>{{student.id}}</td>
+        <td>{{student.Name}}</td>
+        <td>{{student.PresentAddress}}</td>
+        <td>{{student.RollNumber}}</td>
+        <td>{{student.ContactNo}}</td>
+        <td><button class="btn btn-warning v-on:click=UpdateStudent(student.Id)">Update</button></td>
+        <td><button class="btn btn-danger  v-on:click=DeleteStudentByRoll(student.RollNumber)">Delete</button></td>
+        </tr>
+    </tbody>
+</table>
+</div>
+</template>
+<script>
+
+import StudentDataService from '../StudentDataService';
+
+export default{
+
+    name:"Students",
+    data(){
+     return{
+        students:[],
+        message:"",
+     };
+    },
+    methods:{
+        refreshStudents(){
+            StudentDataService.retrieveAllStudents().then((res)=>{
+
+                this.students=res.data;
+            });
+        },
+    },
+    created(){
+        this.refreshStudents();
+    },
+
+};
+</script>
 <style></style>
