72c4a4ebec86e11f0460a6d8addae24955635157
Administrator <Administrator@SUJANHOME>
2024-02-25 15:23:30 +0600

    asdsad

--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,3 +1,33 @@
-<template></template>
+<template>
+<div class="container">
+    <h3>All Employees</h3>
+    <div v-if="message" class="alert alert-success">{{this.message}}</div>
+<div>
+<button class="btn btn-success" v-on:click="addEmployee()">Add Employee</button>
+<template>
+    <router-view></router-view>
+</template>
+</div>
+
+<br/>
+<table class="table table-bordered">
+
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Present Address</th>
+            <th>Phone Number</th>
+            <th>National ID</th>
+            <th>Employee Type</th>
+        </tr>
+    </thead>
+</table>
+
+</div>
+
+
+</template>
 <script></script>
 <style></style>
